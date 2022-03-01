943f8d6518caa1cd27bef7ba7055e435e30780c0
jackpunt <jpeckj@gmail.com>
2022-02-28 23:11:48 -0800

    remove createjs-lib files

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,3 @@
 export * from './basic-intfs'
 export * from './functions'
 export * from './undo'
-
-export * from './createjs-functions'
-export * from './dragger'
-export * from './dropdown'
-export * from './key-binder'
-export * from './param-gui'
-export * from './scaleable-container'
-export * from './edit-box'
-export * from './value-counter'
-
-//import { Stage, Container, DisplayObject, Graphics, Point, Matrix2D, Event, EventDispatcher, MouseEvent, Rectangle, Shape, Text } from 'createjs-module'
-//export { Stage, Container, DisplayObject, Graphics, Point, Matrix2D, Event, EventDispatcher, MouseEvent, Rectangle, Shape, Text }
-//export { Stage, Container, DisplayObject, Graphics, Point, Matrix2D, Event, EventDispatcher, MouseEvent, Rectangle, Shape, Text } from '../../../../../ts/common-lib/src/index'
